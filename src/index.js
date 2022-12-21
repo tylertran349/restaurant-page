982a3eccab4b2b1a6106530052027e9be25fb9a0
tylertran349 <tylertran349@gmail.com>
2022-12-20 21:47:39 -0800

    Make small tweaks

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import { drawHomePage } from '../dist/modules/home'
 import { drawMenuPage } from '../dist/modules/menu'
 import { drawContactPage } from '../dist/modules/contact'
 
-drawMenuPage(); // Load home page on page load
+drawHomePage(); // Load home page on page load
